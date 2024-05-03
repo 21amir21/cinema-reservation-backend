c3b276140f640dcfa0ecafcae6297774a2b4404f
assarhegazy <125737782+Assarhegazy@users.noreply.github.com>
2024-05-03 03:12:06 +0300

    validated all fields in the add movie form

--- validations/AddMovieForm.js
+++ validations/AddMovieForm.js
@@ -1,12 +1,22 @@
 // All  input values in form
 const form = document.querySelector("#submit-form");
 const posterInput = document.getElementById("poster");
+const backgroundimageInput = document.getElementById("backgroundimage");
 const trailerInput = document.getElementById("trailer");
 const movienameInput = document.getElementById("moviename");
+const directornameInput = document.getElementById("directorname");
 const releasedateInput = document.getElementById("releasedate");
-const genreInput = document.getElementById("genre");
+const genre1Input = document.getElementById("genre1");
+const genre2Input = document.getElementById("genre2");
+const genre3Input = document.getElementById("genre3");
+const ratingInput = document.getElementById("rating");
+const ageratingInput = document.getElementById("agerating");
 const runningtimeInput = document.getElementById("runningtime");
+const languageInput = document.getElementById("language");
+const subtitleInput = document.getElementById("subtitles");
 const descriptionInput = document.getElementById("description");
+const shortdescriptionInput = document.getElementById("shortdescription");
+const priceInput = document.getElementById("price");
 
 // Function to display error messages
 const showError = (field, errorText) => {
@@ -23,12 +33,22 @@ const handleFormData = (e) => {
 
   // trimmed values from inputs
   const poster = posterInput.value.trim();
+  const backgroundimage = backgroundimageInput.value.trim();
   const trailer = trailerInput.value.trim();
   const moviename = movienameInput.value.trim();
+  const directorname = directornameInput.value.trim();
   const releasedate = releasedateInput.value.trim();
-  const genre = genreInput.value.trim();
+  const genre1 = genre1Input.value.trim();
+  const genre2 = genre2Input.value.trim();
+  const genre3 = genre3Input.value.trim();
+  const rating = ratingInput.value.trim();
+  const agerating = ageratingInput.value.trim();
   const runningtime = runningtimeInput.value.trim();
+  const language = languageInput.value.trim();
+  const subtitle = subtitleInput.value.trim();
   const description = descriptionInput.value.trim();
+  const shortdescription = shortdescriptionInput.value.trim();
+  const price = priceInput.value.trim();
 
   // Clearing previous error messages
   document
@@ -42,25 +62,53 @@ const handleFormData = (e) => {
   if (poster === "") {
     showError(posterInput, "Poster Required");
   }
+  if (backgroundimage === "") {
+    showError(backgroundimageInput, "Background Image Required");
+  }
   if (trailer === "") {
     showError(trailerInput, "Trailer Required");
   }
   if (moviename === "") {
     showError(movienameInput, "Movie Name Required");
   }
-
+  if (directorname === "") {
+    showError(directornameInput, "Director Name Required");
+  }
   if (releasedate === "") {
     showError(releasedateInput, "Release Date Required");
   }
-  if (genre === "") {
-    showError(genreInput, "Genre Required");
+  if (genre1 === "") {
+    showError(genre1Input, "Genre Required");
+  }
+  if (genre2 === "") {
+    showError(genre2Input, "Genre Required");
+  }
+  if (genre3 === "") {
+    showError(genre3Input, "Genre Required");
+  }
+  if (rating === "") {
+    showError(ratingInput, "Rating Required");
+  }
+  if (agerating === "") {
+    showError(ageratingInput, "Age Rating Required");
   }
   if (runningtime === "") {
     showError(runningtimeInput, "Running Time Required");
   }
-
+  if (language === "") {
+    showError(languageInput, "Language Required");
+  }
+  if (subtitle === "") {
+    showError(subtitleInput, "Subtitle Required");
+  }
   if (description === "") {
     showError(descriptionInput, "Description Required");
+  }
+  if (shortdescription === "") {
+    showError(shortdescriptionInput, "Description Required");
+  }
+  if (price === "") {
+    showError(priceInput, "Price Required");
   }
 
   // Checking for any remaining errors before form submission
